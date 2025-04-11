652b659d89f1b8ee6d10bacbf739395a281276b1
Jhonma <jhonma1238@gmail.com>
2025-04-11 16:51:45 -0500

    Agregamos el LoginControlador

--- Hospital_Code/src/main/java/com/hospital/controlador/LoginControlador.java
+++ Hospital_Code/src/main/java/com/hospital/controlador/LoginControlador.java
@@ -1,10 +1,28 @@
 package com.hospital.controlador;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hospital.modelo.entidad.UsuarioSesion;
+import com.hospital.modelo.servicio.IUsuarioSesionServicio;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginControlador {
+    private IUsuarioSesionServicio servicio;
 
+    @PostMapping("/usuario/guardar")
+    public void guardar(@RequestBody UsuarioSesion usuario){
+        servicio.guardar(usuario);
+    }
 
+    @GetMapping("/usuario/buscar")
+    public ResponseEntity<?> buscar(@PathVariable String id){
+        UsuarioSesion usuario=servicio.buscarByUsuario(id);
+        if (usuario!=null){
+            return ResponseEntity.ok(usuario);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
+        }
+    }
 }
