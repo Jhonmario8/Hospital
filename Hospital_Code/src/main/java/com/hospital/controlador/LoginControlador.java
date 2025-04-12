0009e6836d414fcde35c5f515618293fd29fddfe
Jhonma <jhonma1238@gmail.com>
2025-04-12 14:13:34 -0500

    hicimos el fetch de el login

--- Hospital_Code/src/main/java/com/hospital/controlador/LoginControlador.java
+++ Hospital_Code/src/main/java/com/hospital/controlador/LoginControlador.java
@@ -3,26 +3,39 @@ package com.hospital.controlador;
 
 import com.hospital.modelo.entidad.UsuarioSesion;
 import com.hospital.modelo.servicio.IUsuarioSesionServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginControlador {
+    @Autowired
     private IUsuarioSesionServicio servicio;
 
     @PostMapping("/usuario/guardar")
     public void guardar(@RequestBody UsuarioSesion usuario){
         servicio.guardar(usuario);
     }
-
-    @GetMapping("/usuario/buscar")
-    public ResponseEntity<?> buscar(@PathVariable String id){
-        UsuarioSesion usuario=servicio.buscarByUsuario(id);
-        if (usuario!=null){
-            return ResponseEntity.ok(usuario);
-        }else{
+    @GetMapping("/usuario/search/{id}")
+    public ResponseEntity<?> search(@PathVariable String id){
+        UsuarioSesion usu=servicio.buscarByUsuario(id);
+        if (usu!=null){
+            return ResponseEntity.ok(usu);
+        }else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
         }
+    }
+
+    @PostMapping("/usuario/buscar")
+    public ResponseEntity<?> buscar(@RequestBody UsuarioSesion usu) {
+        UsuarioSesion usuario = servicio.buscarByUsuario(usu.getUsuario());
+
+        if (usuario != null) {
+            if (usuario.getContraseña().equals(usu.getContraseña())) {
+                return ResponseEntity.ok(usuario);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El usuario o la contraseña no coinciden");
     }
 }
---
+++ Hospital_Code/src/main/resources/static/Scripts/Login/InicioSesion.js
@@ -0,0 +1,25 @@
+const iniciarBtn=document.getElementById("iniciarBtn")
+
+iniciarBtn.addEventListener("click", async e => {
+    e.preventDefault()
+    const usuario = document.getElementById("user").value
+    const contraseña = document.getElementById("contraseña").value
+    try {
+        let res = await fetch(`http://localhost:8080/usuario/buscar`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({usuario: usuario, contraseña: contraseña})
+        })
+        if (res.status === 404) {
+            alert("Usuario o contraseña incorrectos")
+        }
+        if (!res.ok) {
+            throw new Error("Error al obtener el usuario")
+        }
+        let json = await res.json()
+        console.log("Usuario autenticado:", json);
+        window.location.href = "../../html/index.html"
+    } catch (e) {
+        console.error(e)
+    }
+})
