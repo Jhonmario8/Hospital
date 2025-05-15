e983572f5acb516aec14b95367f9fa3d8e3a92ab
Jhonma <jhonma1238@gmail.com>
2025-05-15 15:11:03 -0500

    buscaHabitacion: añadimos botones para editar y borrar

--- Hospital_Code/src/main/resources/static/Scripts/Habitaciones/buscarHabitacion.js
+++ Hospital_Code/src/main/resources/static/Scripts/Habitaciones/buscarHabitacion.js
@@ -1,13 +1,111 @@
 const buscarBtn=document.getElementById("buscarBtn")
 let tabla=document.getElementById("habitaciones-tabla")
+const form = document.querySelector("form")
 function crearFila(hab){
     let row=document.createElement("tr")
     row.innerHTML=`
     <td>${hab.numHabitacion}</td>
     <td>${hab.tipoHabitacion}</td>
     <td>${hab.capacidad}</td>
+    <td ><button class="editar">Editar</button> <button class="borrar">Borrar</button></td>
     `
+    row.querySelector(".editar").addEventListener("click",async e=>{
+        e.preventDefault()
+        const id=hab.numHabitacion
+        document.querySelector("table").innerHTML=""
+        try{
+            let response= await fetch(`http://localhost:8080/habitaciones/buscar/${id}`)
+            if (response.status===404){
+                alert("El id ingresado no esta registrado")
+                return
+            }
+            if (!response.ok){
+                throw new Error("Error al buscar la habitacion")
+            }
+            let json=await response.json()
+            form.innerHTML=""
+            let [labelT,tipo]=crearInput("tipo","Tipo: ",json.tipoHabitacion)
+            let [labelC,capacidad]=crearInput("capacidad","Capacidad: ",json.capacidad)
+
+            const actualizarBtn=document.createElement("button")
+            actualizarBtn.textContent="Actualizar"
+
+            const volverBtn=document.createElement("a")
+            volverBtn.textContent="Volver"
+            volverBtn.setAttribute("class","button")
+            volverBtn.setAttribute("href","../../html/GestionHabitaciones/buscahabitacion.html")
+            actualizarBtn.addEventListener("click",async e=>{
+                e.preventDefault()
+                try {
+                    let res=await fetch(`http://localhost:8080/habitaciones/actualizar`,{
+                        method:"PUT",
+                        headers:{"Content-Type":"application/json"},
+                        body:JSON.stringify({
+                            numHabitacion: id,
+                            tipoHabitacion: tipo.value,
+                            capacidad: capacidad.value
+                        })
+                    })
+                    if (!res.ok){
+                        throw new Error("Error al actualizar la habitacion")
+                    }
+                    alert("Habitacion actualizada correctamente")
+                    window.location.reload()
+                }catch (e){
+                    console.error(e)
+                }
+            })
+
+            form.appendChild(labelT)
+            form.appendChild(tipo)
+            form.appendChild(labelC)
+            form.appendChild(capacidad)
+            form.appendChild(actualizarBtn)
+            form.appendChild(volverBtn)
+
+        }catch (e){
+            console.error(e)
+        }
+    })
+    row.querySelector(".borrar").addEventListener("click",async e=>{
+        e.preventDefault()
+        const id=hab.numHabitacion
+        try{
+            let response=await fetch(`http://localhost:8080/habitaciones/buscar/${id}`)
+            if (response.status===404){
+                alert("La habitacion no se encontro")
+                return
+            }
+            if (!response.ok){
+                throw new Error("Error al buscar la habitacion")
+            }
+            let conf=confirm("Seguro que desea borrar la habitacion?")
+            if (conf) {
+                let res = await fetch(`http://localhost:8080/habitaciones/borrar/${id}`, {
+                    method: "DELETE"
+                })
+                if (!res.ok) {
+                    throw new Error("Error al borrar la habitacion")
+                }
+                alert("Habitacion eliminada")
+                window.location.reload()
+            }
+        }catch (e){
+            console.error(e)
+        }
+    })
     return row
+}
+function crearInput(id,lblTxt,value){
+    const label=document.createElement("label")
+    label.setAttribute("for",id)
+    label.textContent=lblTxt
+    const input=document.createElement("input")
+    input.setAttribute("id",id)
+    input.setAttribute("required",true)
+    input.value=value
+
+    return [label,input]
 }
 async function mostrar(){
     try{
